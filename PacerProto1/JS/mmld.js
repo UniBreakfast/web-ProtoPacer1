9fab996fcdf792421c67f64a87e916e738abac22
unibreakfast <unibreakfast1@gmail.com>
2018-10-09 21:38:22 +0300

    get.php works on page load.

--- PacerProto1/JS/mmld.js
+++ PacerProto1/JS/mmld.js
@@ -1,7 +1,13 @@
 const mmld = function main_menu_load_data() {
-  var user_id = f.APIcookie.get('user').split('|')[0];
-
-
+  if (f.APIcookie.get('user')) {
+    var user_id = f.APIcookie.get('user').split('|')[0];
+    function place_ud(response, login, confidence) {
+      [login, confidence] = response.split('|');
+      document.getElementById('user')      .innerHTML = login;
+      document.getElementById('confidence').innerHTML = confidence;
+    }
+    f.POST('PHP/get.php?userid='+user_id, place_ud, alert);
+  }
 }
 
 mmld();
--- PacerProto1/PHP/get.php
+++ PacerProto1/PHP/get.php
@@ -1,16 +1,19 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'sandbox.php';
+if (isset($_REQUEST['userid']) and trim($_REQUEST['userid']!=='')) {
+  $userid = trim($_REQUEST['userid']);
 
-$userid = ;
-
-$query = "SELECT login, confidence FROM test_users
+  $query = "SELECT login, confidence FROM test_users
           WHERE id = $userid";
-$result = mysqli_query($db, $query)
-  or exit ('SELECT login, confidence Query failed!');
+  $result = mysqli_query($db, $query)
+    or exit ('SELECT login, confidence Query failed!');
 
-if ($result) {
-  list($login, $confidence) = mysqli_fetch_row($result);
-  echo "$login|$confidence";
+  if ($result) {
+    list($login, $confidence) = mysqli_fetch_row($result);
+    echo "$login|$confidence";
+  }
+  else echo "no user found with user_id $userid";
 }
+else echo "no user_id received to get data by";
 
 ?>
